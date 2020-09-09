7cad2d13f3138bd7741f170e6c189216ecd9ddbb
gungor <gungor.yilmaz@vodafone.com>
2020-09-09 06:54:55 +0300

    spring-boot-starter-web excluded due to webflux

--- src/main/java/webclient/soap/client/ReactiveSoapClient.java
+++ src/main/java/webclient/soap/client/ReactiveSoapClient.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
